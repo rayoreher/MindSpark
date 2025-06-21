fce159005e488b744144861eea0dac3e9bb8e5a2
Ray Orellana <rayoreher@gmail.com>
2025-06-21 16:10:16 +0200

    feat: upfdated vite config 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/MindSpark/',
@@ -9,6 +8,7 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 });
